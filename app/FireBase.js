ae89df3ecd8bba9fe754a14966c53b52ebc2321c
Giovanni Mugno <49875306+LeoJuanZ@users.noreply.github.com>
2020-10-16 19:02:44 -0400

    Update FireBase.js

--- app/FireBase.js
+++ app/FireBase.js
@@ -1,16 +1,31 @@
 import * as firebase from 'firebase';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCXBnSa7UUFriUjwCCbMXGTMWMfpSR0laY",
-    authDomain: "crud-react-native-f94e0.firebaseapp.com",
-    databaseURL: "https://crud-react-native-f94e0.firebaseio.com",
-    projectId: "crud-react-native-f94e0",
-    storageBucket: "crud-react-native-f94e0.appspot.com",
-    messagingSenderId: "828914160449",
-    appId: "1:828914160449:web:5c43bccb6862209ded87c0",
-    measurementId: "G-V0Q9M70XN8"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB0IyDMEq4JXjxHyCugCcMoTIqG73zeh2Y",
+    authDomain: "codenesta-2ea69.firebaseapp.com",
+    databaseURL: "https://codenesta-2ea69.firebaseio.com",
+    projectId: "codenesta-2ea69",
+    storageBucket: "codenesta-2ea69.appspot.com",
+    messagingSenderId: "325263210693",
+    appId: "1:325263210693:web:f5e299af9e88f51a5158ac",
+    measurementId: "G-ZV47TS4JD2"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCXBnSa7UUFriUjwCCbMXGTMWMfpSR0laY",
+//     authDomain: "crud-react-native-f94e0.firebaseapp.com",
+//     databaseURL: "https://crud-react-native-f94e0.firebaseio.com",
+//     projectId: "crud-react-native-f94e0",
+//     storageBucket: "crud-react-native-f94e0.appspot.com",
+//     messagingSenderId: "828914160449",
+//     appId: "1:828914160449:web:5c43bccb6862209ded87c0",
+//     measurementId: "G-V0Q9M70XN8"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
 
   export default firebase;
